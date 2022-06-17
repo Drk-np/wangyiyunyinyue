7a797b4513ea8e47a4c08b039d850d064a844239
drk <2442854604@qq.com>
2022-06-17 10:19:10 +0800

    更新接口信息

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,7 +6,7 @@ import {Message} from 'element-ui';
 //1. 创建新的axios实例，
 const service = axios.create({
     // 公共接口--这里注意后面会讲
-    baseURL: 'http://81.68.158.36:3000',
+    baseURL: 'http://8.141.170.231:8001',
     // 超时时间 单位是ms，这里设置了3s的超时时间
     timeout: 3 * 1000
 })
